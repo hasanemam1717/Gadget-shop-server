a828b77ae81e81b5ec93d8bd25993a56f2bcf9f0
hasanemam1717 <hasanemam1717@gmail.com>
2024-11-18 14:12:57 +0600

    solved jwt problem and google auth providar problem

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const app = express();
+require("dotenv").config();
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
-require("dotenv").config();
-const app = express();
-const port = process.env.PORT || 4000;
+const jwt = require("jsonwebtoken");
+
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
@@ -21,20 +23,51 @@ const client = new MongoClient(uri, {
   },
 });
 
+const userCollection = client.db("gadgetShop").collection("users");
+const productCollection = client.db("gadgetShop").collection("products");
+
 const dbConnect = async () => {
-    try{
-        client.connect()
-        console.log("Database connected successfully");
-    }
-    catch(error){
-        console.log(error?.name,error?.massage);
-    }
+  try {
+    client.connect();
+    console.log("Database connected successfully");
+    // insertUser
+    app.post("/users", async (res, req) => {
+      const user = req.body;
+      console.log(user);
+      return;
+      const result = await userCollection.insertOne(user);
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ massage: "User already exist" });
+      }
+      res.send(result);
+    });
+  } catch (error) {
+    console.log(error?.name, error?.massage);
+  }
 };
 
-dbConnect()
+dbConnect();
 //  api
 app.get("/", (req, res) => {
   res.send("Server is running");
+});
+
+//jwt api generate
+app.post("/authentication", (req, res) => {
+  const { email } = req.body;
+
+  if (!email) {
+    return res.status(400).send({ error: "Email is required" });
+  }
+
+  // Generate token
+  const token = jwt.sign({ email }, process.env.ACCESS_key_token, {
+    expiresIn: "10d",
+  });
+
+  res.status(200).send({ token });
 });
 
 app.listen(port, () => {
